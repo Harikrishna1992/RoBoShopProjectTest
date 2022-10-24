3b6951514b4aec8a46a3b897847b03ed95aa7142
Hari <Pamidalahari6@gmail.com>
2022-10-24 13:12:33 +0530

    modified

---
+++ LinuxShellPractice/First Script.sh
@@ -0,0 +1,56 @@
+#! /bin/sh
+
+Echo -e "\e[31mThis ia my first script\e[m"
+
+Echo -n ""
+
+A = 10
+B= 20
+Hari = "My fist name"
+
+readonly Hari
+
+Login Users = $(uname -o)
+Function Name 
+{
+    echo "First function"
+}
+
+echo -e "Hi, please type the word: \c "
+read  word
+echo "The word you entered is: $word"
+#$!
+#$#
+
+Names = ('Pamidala' 'Hari' 'krishna')
+Elements = $($Name[@])
+
+Name[0] = "Pamidala"
+Name[1] = "hari"
+#For Loop
+For (i=0;i<Elements;i++);do
+echo $(Names[$(i)])
+done
+#If
+if[];then
+fi
+#If Else
+if[];then
+Else 
+fi
+#Nested if
+if[];then
+elif[];then
+Else
+fi
+exit ;
+#Loops
+For $n in $arry ; do 
+
+done
+
+case $Variable in 
+
+1) "";
+2) "";
+esac
---
+++ LinuxShellPractice/Hello World.sh
@@ -0,0 +1,36 @@
+#!/bin/sh
+
+echo "Hello World"
+
+echo -e "\e[32m hello world \e[0m"
+echo -e "\e[33m I am printing Yellow \e[0m"
+echo -n "Hello World :"
+$!
+
+Name = Hari
+Readonly Name
+Name = Hari P
+
+echo "$name"
+Test = testing
+unset Test
+echo "$Test"
+
+#$0 File name of script
+For arg in $*
+do
+echo "print $arg"
+
+done
+
+echo $*   # Gives you all the arguments used in the script
+echo $@   # Gives you all the arguments used in the script
+echo $#   # Gves you the number of arguments users
+echo $$   # Gives you the PID of the current process
+echo $?   # Gives you the exit code the previous command
+echo -e "Hi, please type the word: \c "
+read  word
+echo "The word you entered is: $word" 
+
+
+echo $name

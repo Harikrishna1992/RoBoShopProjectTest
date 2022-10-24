49871c22a55cb4bde65855dec9369c4250b3e797
Hari <Pamidalahari6@gmail.com>
2022-10-24 11:13:23 +0530

    modified

--- Linux Shell Practice/Hello World.sh
+++ Linux Shell Practice/Hello World.sh
@@ -2,7 +2,16 @@
 
 echo "Hello World"
 
-echo -e "/e[32m hello world /e[0m"
-
+echo -e "\e[32m hello world \e[0m"
+echo -e "\e[33m I am printing Yellow \e[0m"
 echo -n "Hello World :"
 $!
+
+Name = $1
+
+echo -e "Hi, please type the word: \c "
+read  word
+echo "The word you entered is: $word" 
+
+
+echo $name

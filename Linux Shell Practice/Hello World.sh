dd7eb5cb58c0885b0b019c34e1bf304af7d07611
Hari <Pamidalahari6@gmail.com>
2022-10-24 13:07:21 +0530

    modified

--- Linux Shell Practice/Hello World.sh
+++ Linux Shell Practice/Hello World.sh
@@ -7,8 +7,27 @@ echo -e "\e[33m I am printing Yellow \e[0m"
 echo -n "Hello World :"
 $!
 
-Name = $1
+Name = Hari
+Readonly Name
+Name = Hari P
 
+echo "$name"
+Test = testing
+unset Test
+echo "$Test"
+
+#$0 File name of script
+For arg in $*
+do
+echo "print $arg"
+
+done
+
+echo $*   # Gives you all the arguments used in the script
+echo $@   # Gives you all the arguments used in the script
+echo $#   # Gves you the number of arguments users
+echo $$   # Gives you the PID of the current process
+echo $?   # Gives you the exit code the previous command
 echo -e "Hi, please type the word: \c "
 read  word
 echo "The word you entered is: $word" 

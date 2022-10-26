bd932fac9923c434280fde87788cd496d03c987d
Hari <Pamidalahari6@gmail.com>
2022-10-26 19:05:55 +0530

    adding

--- LinuxShellPractice/FroentEnd.sh
+++ LinuxShellPractice/FroentEnd.sh
@@ -28,12 +28,10 @@ echo  "Downloading the front end content"
 curl -s -L -o /tmp/$COMPONENT.zip "https://github.com/stans-robot-project/frontend/archive/main.zip" &>> $LOG
 
 if [ $? -eq 0 ] ; then
-echo -e "\e[33m $FRONTENDSERVICE is Sucess\e[0m"
+echo -e "\e[33m $COMPONENT extracted is Sucess\e[0m"
 else
-echo -e "\e[31m $FRONTENDSERVICE is failure\e[0m"
+echo -e "\e[31m $COMPONENT extracted is failure\e[0m"
 fi
-
-
 
 CheckTheStatus()
 {
@@ -47,25 +45,25 @@ echo -n "remove the content of nginx before deploy the nginx : "
 rm -rf "/usr/share/nginx/html/*" &>> $LOG
  CheckTheStatus $?
 
-echo -n "unziping the $Component content into /tmp/$Component.zip"
+echo -n "unziping the $COMPONENT content into /tmp/$COMPONENT.zip"
 
-unzip /tmp/$Component.zip -o -q &>> $LOG
+unzip /tmp/$COMPONENT.zip -o -q &>> $LOG
 
 CheckTheStatus $?
 cd /usr/share/nginx/html &>> $LOG
-mv /tmp/$Component-main/* .&>> $LOG
+mv /tmp/$COMPONENT-main/* .&>> $LOG
 mv static/* . &>> $LOG
 rm -rf frontend-main README.md &>> $LOG
 mv localhost.conf /etc/nginx/default.d/roboshop.conf &>> $LOG
 
-echo -n "$FrontEndService is enabled"
-systemctl enable $FrontEndService &>> $LOG
+echo -n "$FRONTENDSERVICE is enabled"
+systemctl enable $FRONTENDSERVICE &>> $LOG
 CheckTheStatus $?
-if [$? -eq 0] ; then
-echo -e "\e[32m $FrontEndService is Sucess\e[0m"
+if [ $? -eq 0 ] ; then
+echo -e "\e[32m $FRONTENDSERVICE is Sucess\e[0m"
 else
-echo -e "\e[31m $FrontEndService is failure\e[0m"
+echo -e "\e[31m $FRONTENDSERVICE is failure\e[0m"
 fi
-echo -n "$FrontEndService is restarted"
-Systemctl restart $FrontEndService &>> $LOG
+echo -n "$FRONTENDSERVICE is restarted"
+Systemctl restart $FRONTENDSERVICE &>> $LOG
 CheckTheStatus $?

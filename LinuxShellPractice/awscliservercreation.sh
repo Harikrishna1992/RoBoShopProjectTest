8c28c35a82df770ba53f9556dc618b5cacc26c50
Hari <Pamidalahari6@gmail.com>
2022-11-01 15:20:20 +0530

    creation

--- LinuxShellPractice/awscliservercreation.sh
+++ LinuxShellPractice/awscliservercreation.sh
@@ -1,6 +1,7 @@
 #!bin/bash
 
-AMIID = "$(aws ec2 describe-images --filters "Name=name,Values=DevOps-LabImage-CentOS7" | jq '.Images[].ImageId' |sed -e's/"//g')"
+AMIID="$(aws ec2 describe-images --filters "Name=name,Values=DevOps-LabImage-CentOS7" | jq '.Images[].ImageId' |sed -e's/"//g')"
 echo $AMIID
+securitygroup=aws ec2 describe-security-groups --filters Name=group-name,Values=Test-Allow-All
 
-aws ec2 run-instances --image-id $AMIID --instance-type t2.micro --tag-specifications 'ResourceType=instance,Tags=[{Key=webserver,Value=production}]'
+aws ec2 run-instances --image-id $AMIID --instance-type t2.micro --tag-specifications "ResourceType=instance,Tags=[{Key=Name,Value=Test}]" --security-group-ids $securitygroup

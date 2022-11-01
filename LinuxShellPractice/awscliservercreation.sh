232bbbbffd2f035d6034b59ab3cdd0114d5fa79d
Hari <Pamidalahari6@gmail.com>
2022-11-01 14:36:17 +0530

    creation

--- LinuxShellPractice/awscliservercreation.sh
+++ LinuxShellPractice/awscliservercreation.sh
@@ -1,7 +1,6 @@
 #!bin/bash
 
-AMIID = "$(aws ec2 describe-images --filters Name=name,Values=DevOps-LabImage-CentOS7 | jq .Images[].ImageId |sed -e's/"//g')"
-
+AMIID = "$(aws ec2 describe-images --filters "Name=name,Values=DevOps-LabImage-CentOS7" | jq '.Images[].ImageId' |sed -e's/"//g')"
 echo $AMIID
 
 aws ec2 run-instances --image-id $AMIID --instance-type t2.micro --tag-specifications 'ResourceType=instance,Tags=[{Key=webserver,Value=production}]'

afd07dee6466d094fabb6dc78554b8ab8028b5df
Hari <Pamidalahari6@gmail.com>
2022-11-01 18:03:02 +0530

    creation

--- LinuxShellPractice/awscliservercreation.sh
+++ LinuxShellPractice/awscliservercreation.sh
@@ -1,7 +1,11 @@
 #!bin/bash
-
+set -e
+COMPONENT=$1
+if [ -z -ne $1 ];then
+echo "Please pass argument to $0 for creating webserver \n\t eg. $0 Frontend"
+fi
 AMIID="$(aws ec2 describe-images --filters "Name=name,Values=DevOps-LabImage-CentOS7" | jq '.Images[].ImageId' |sed -e's/"//g')"
 echo $AMIID
-securitygroup=aws ec2 describe-security-groups --filters Name=group-name,Values=Test-Allow-All
-
-aws ec2 run-instances --image-id $AMIID --instance-type t2.micro --tag-specifications "ResourceType=instance,Tags=[{Key=Name,Value=Test}]" --security-group-ids $securitygroup
+securitygroup=aws ec2 describe-security-groups --filters Name=group-name,Values=Test-Allow-All | jq '.SecurityGroups[].GroupId' | sed 's\"\\g'
+    
+aws ec2 run-instances --image-id $AMIID --instance-type t2.micro --tag-specifications "ResourceType=instance,Tags=[{Key=Name,Value=$COMPONENT}]" --security-group-ids $securitygroup | jq

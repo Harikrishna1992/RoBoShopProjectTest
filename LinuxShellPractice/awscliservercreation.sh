382b83fc403e0a9818894acf0c143d7417718ebc
Hari <Pamidalahari6@gmail.com>
2022-11-01 14:25:23 +0530

    creation

---
+++ LinuxShellPractice/awscliservercreation.sh
@@ -0,0 +1,7 @@
+#!bin/bash
+
+AMIID = $(aws ec2 describe-images --filters Name=name,Values=DevOps-LabImage-CentOS7 | jq .Images[].ImageId |sed -e's/"//g')
+
+echo $AMIID
+
+aws ec2 run-instances --image-id $AMIID --instance-type t2.micro --tag-specifications 'ResourceType=instance,Tags=[{Key=webserver,Value=production}]'
